8ba4159f5d16fa393eb9a908559b5b8b3c653cf8
Dsahrath-Metaversity <dashrath@metaversitytechnologies.com>
2023-10-11 13:12:03 +0530

    add:modal

---
+++ src/component/modal/Modal.jsx
@@ -0,0 +1,20 @@
+import { Dialog } from '@mui/material';
+import Modal from '@mui/material/Modal';
+
+const ModalComponent = ({ Elememt, open, setOpen }) => {
+  const handleClose = () => setOpen(false);
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={() => handleClose()}
+        // aria-labelledby="modal-modal-title"
+        // aria-describedby="modal-modal-description"
+      >
+        {Elememt}
+      </Dialog>
+    </>
+  )
+}
+
+export default ModalComponent
---
+++ src/component/topBanner/TopBanner.jsx
@@ -0,0 +1,11 @@
+///styles
+import "./styles.scss"
+const TopBanner = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TopBanner
