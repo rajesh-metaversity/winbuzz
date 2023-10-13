53517746e70fb4927567968fabab8175fc661ced
Dsahrath-Metaversity <dashrath@metaversitytechnologies.com>
2023-10-13 11:52:58 +0530

    title

--- src/component/inPlay/InplayCollapse.jsx
+++ src/component/inPlay/InplayCollapse.jsx
@@ -1,22 +1,11 @@
-import ball from "../../assets/img/ball.png";
+
 import InPlayOddRow from "./InPlayOddRow";
+import Title from "./Title";
 const InplayCollapse = () => {
   return (
     <>
       <div className="in-play-collapse-container">
-        <div className="inplay-titile-box">
-          <div className="inplay-title-left-col">
-            <img src={ball} alt="" />
-            Cricket
-          </div>
-          <div className="inplay-title-right-col">
-            <ul>
-              <li>1</li>
-              <li>x</li>
-              <li>2</li>
-            </ul>
-          </div>
-        </div>
+       <Title name={"Cricket"}/>
         <div className="odd-container">
           {["liv2", "live","livw"].map((res) => {
             return (
