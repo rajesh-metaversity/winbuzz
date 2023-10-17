e5533d0e4e95db4a3fd1f7c02151ec3fb66dac44
himanshu-metaversity <himanshu@metaversitytechnologies.com>
2023-10-17 11:59:39 +0530

    himanshu

--- src/pages/gameDeatail/GameDetail.jsx
+++ src/pages/gameDeatail/GameDetail.jsx
@@ -1,19 +1,19 @@
 import Iframes from '../../component/Iframes/Iframes'
 import RightTable from './RightTable'
 import RightTableBottom from './RightTableBottom'
-import './style.scss'
+import './styles.scss'
 
 const GameDetail = () => {
   return (
-      <div className='game_detail-cont'>
-          <div className="game-detail-left-col">
-           <Iframes/>
-          </div>
-          <div className="game-detail-right-col">
-              <RightTable />
-              <RightTableBottom />
+    <div className='game_detail-cont'>
+      <div className="game-detail-left-col">
+        <Iframes />
+      </div>
+      <div className="game-detail-right-col">
+        <RightTable />
+        <RightTableBottom />
 
-            </div>
+      </div>
     </div>
   )
 }
