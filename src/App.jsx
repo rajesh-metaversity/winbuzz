2f19cf60ff6f865526b32438aab3d73e3152d13f
= <=>
2023-11-30 18:03:03 +0530

    solveError

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,47 @@ import Routes from "./routes/Routes";
 import { useEffect } from "react";
 import { isLoginSelector, setIslogin } from "./App/LoginSlice";
 import { useQtechAuthQuery } from "./Services/Qtech/Qtech";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useValidateJWTMutation } from "./Services/ValidateJWT/Validate";
 
 function App() {
-	const [trigger, { data, isLoading, isError }] = useValidateJWTMutation();
-	const dispatch = useDispatch();
+  const [trigger, { data, isLoading, isError }] = useValidateJWTMutation();
+  const dispatch = useDispatch();
 
-	const isLogin = useSelector(isLoginSelector);
-	
-	useEffect(() => {
-		if (localStorage.getItem('session')) {
-			dispatch(setIslogin(true));
-		}
-	}, []);
+  const isLogin = useSelector(isLoginSelector);
 
-	useEffect(() => {
-		const validateJWT = setInterval(() => {
-			dispatch(trigger({}))
-		}, 5000)
-		return () => clearInterval(validateJWT)
-	}, [])
+  useEffect(() => {
+    if (localStorage.getItem("session")) {
+      dispatch(setIslogin(true));
+    }
+  }, []);
 
-	return (
-		<>
-			<ToastContainer />
-			{isLogin && <IfLoginComp />}
-			<Routes />
-		</>
-	);
+  useEffect(() => {
+    if (isLogin) {
+      const validateJWT = setInterval(() => {
+        dispatch(trigger({}));
+      }, 5000);
+      return () => clearInterval(validateJWT);
+    }
+  }, []);
+
+  return (
+    <>
+      <ToastContainer />
+      {isLogin && <IfLoginComp />}
+      <Routes />
+    </>
+  );
 }
 
 const IfLoginComp = () => {
-	const { data: qtechAuth } = useQtechAuthQuery(undefined, { pollingInterval: 3000 });
-	useEffect(() => {
-		localStorage.setItem('casino-token', qtechAuth?.data?.access_token);
-	}, [qtechAuth]);
-	return <></>;
+  const { data: qtechAuth } = useQtechAuthQuery(undefined, {
+    pollingInterval: 3000,
+  });
+  useEffect(() => {
+    localStorage.setItem("casino-token", qtechAuth?.data?.access_token);
+  }, [qtechAuth]);
+  return <></>;
 };
 export default App;
