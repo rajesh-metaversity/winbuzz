3167f6a03c65c969ec86159cf560d750d5ed4898
Dhruvmetaversity <dhruvmetaversitytechnologies.com>
2023-12-06 13:11:45 +0530

    Pnl

--- src/pages/BettingProfitLoss/styled.js
+++ src/pages/BettingProfitLoss/styled.js
@@ -5,14 +5,14 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 export const BettingStyled = sytled(DatePicker)(({theme}) => ({
     '& .MuiInputBase-input': {
         padding: "5px",
-        // display: "none"
+        minWidth: "50px"
 
     },
     [theme.breakpoints.down(780)]: {
 		width: '100%',
         color: "#939393",
         border: "1px solid rgba(170,170,170,.3)",
-        // padding: "10px"
+        textAlign: "center",
 	}
     
  
@@ -22,19 +22,43 @@ export const SelectStyles = sytled(Select)(({ theme }) => ({
 	color: 'black',
 	// width: '100%',
     padding: '0px',
-    width: "100px",
-    textAlign: "end",
+    width: "150px",
+    textAlign: "start",
 	'& .MuiInputBase-input': {
 		padding: '5px 14px',
 		width: "100%",
 		marginRight: "10px"
 
+    },
+    [theme.breakpoints.down(450)]: {
+        border: "1px solid rgba(170,170,170,.3)",
+        color: "#939393",
+        whiteSpace: "nowrap",
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+        maxWidth: "210px",
+		width: '100%',
+        // padding: "10px"
+    },
+    [theme.breakpoints.down(700)]: {
+        border: "1px solid rgba(170,170,170,.3)",
+        color: "#939393",
+        whiteSpace: "nowrap",
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+        // maxWidth: "210px",
+		width: '100%',
+        // padding: "10px"
 	},
 	
 	[theme.breakpoints.down(780)]: {
-		width: '100%',
-		border: "1px solid rgba(170,170,170,.3)",
+        border: "1px solid rgba(170,170,170,.3)",
         color: "#939393",
+        // whiteSpace: "nowrap",
+        // overflow: "hidden",
+        // textOverflow: "ellipsis",
+        // maxWidth: "210px",
+		width: '100%',
         // padding: "10px"
 	}
 }));
