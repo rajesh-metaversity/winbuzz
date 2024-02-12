4c033e7b64c626ba120015fbe2e5bd5cd8dea1c5
= <=>
2024-02-12 14:05:27 +0530

    bugs

--- src/component/runAmount/RunAmount.jsx
+++ src/component/runAmount/RunAmount.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from "react";
 import "./styles.scss";
 import { useUserFancyBookQuery } from "../../Services/userFancyBook/UserFancyBook";
+import Loader from "../Loader/Loader";
 
 const RunAmount = ({ data }) => {
-  const { data: pnl } = useUserFancyBookQuery(data);
+  const { data: pnl, isLoading } = useUserFancyBookQuery(data);
 
   // useEffect(() => {
   //   trigger(data);
   // }, []);
 
-  console.log(pnl, "pnl");
   return (
     <div className="run-amount">
       <h2>Run Amount</h2>
@@ -36,40 +36,46 @@ const RunAmount = ({ data }) => {
           Amount
         </h4>
       </div>
-      {pnl?.data?.map((res, index) => {
-        return (
-          <React.Fragment key={res?.pnl + index + 1}>
-            <div
-              className="pnl"
-              style={{
-                display: "flex",
-                border: "1px solid rgb(128 128 128 / 14%)",
-              }}
-            >
-              <p
+      {isLoading ? (
+        <Loader />
+      ) : pnl?.data?.length ? (
+        pnl?.data?.map((res, index) => {
+          return (
+            <React.Fragment key={res?.pnl + index + 1}>
+              <div
+                className="pnl"
                 style={{
-                  width: "200px",
-                  margin: "2px",
-                  paddingLeft: "5px",
-                  color: "rgb(136 154 168)",
-                  fontWeight: "500",
+                  display: "flex",
+                  border: "1px solid rgb(128 128 128 / 14%)",
                 }}
               >
-                {res?.odds}
-              </p>
-              <p
-                style={{
-                  margin: "2px",
-                  color: res?.pnl >= 0 ? "Green" : "red",
-                  fontWeight: "500",
-                }}
-              >
-                {res?.pnl}
-              </p>
-            </div>
-          </React.Fragment>
-        );
-      })}
+                <p
+                  style={{
+                    width: "200px",
+                    margin: "2px",
+                    paddingLeft: "5px",
+                    color: "rgb(136 154 168)",
+                    fontWeight: "500",
+                  }}
+                >
+                  {res?.odds}
+                </p>
+                <p
+                  style={{
+                    margin: "2px",
+                    color: res?.pnl >= 0 ? "Green" : "red",
+                    fontWeight: "500",
+                  }}
+                >
+                  {res?.pnl}
+                </p>
+              </div>
+            </React.Fragment>
+          );
+        })
+      ) : (
+        <p style={{ width: "100%", textAlign: "center" }}>No data</p>
+      )}
     </div>
   );
 };
